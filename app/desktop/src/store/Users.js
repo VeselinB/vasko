f75c114c95d09c559b3ad993ed43c2559a2cd2f9
Vasil Sarbinsky <batcila@gmail.com>
2022-01-18 11:10:08 +0200

    Add gitignore file, Some code changes

--- app/desktop/src/store/Users.js
+++ app/desktop/src/store/Users.js
@@ -2,40 +2,25 @@ Ext.define('newApp.store.Users', {
     extend: 'Ext.data.Store',
     alias: 'store.users',
 
-    fields: [
-        'name',
-        'email',
-        'phone'
-    ],
+    model: 'newApp.model.User',
 
     storeId: 'usersStore',
 
     proxy: {
         type: 'rest',
-        // type: 'ajax',
         url: "http://localhost:3000/users",
+
         limitParam: '_limit',
         pageParam: '_page',
         startParam: '_start',
+
         reader: {
             type: 'json',
-            rootProperty: 'users',
-         //   totalProperty: 'totalCount'
-        
+            rootProperty: 'items',
+            totalProperty: 'totalCount'
         }
     },
-    pageSize: 12,
-    autoLoad: true,
-    listeners: {
-        load: function(a,b,c,d,f){
-          // this.totalCount=10;
-// console.log(Ext.getCmp("usersGrid").down('pagingtoolbar').doRefresh())
-      // this.pagingToolbar.onLoad();
-           // console.log(this)
-        }
-    }
-        
 
-
-
+    pageSize: 5,
+    autoLoad: true
 });
--- app/desktop/src/view/user/grid/UsersGrid.js
+++ app/desktop/src/view/user/grid/UsersGrid.js
@@ -3,10 +3,11 @@ Ext.define('newApp.view.user.grid.Grid', {
     xtype: 'usersgridview',
 
     grouped: false,
+
     id:"usersGrid",
+
     plugins: {
-    //    pagingtoolbar: true,
-      
+       pagingtoolbar: true
     },
   
     columns: [
@@ -15,14 +16,18 @@ Ext.define('newApp.view.user.grid.Grid', {
             dataIndex: 'id',
             editable: true,
             width: 30,
-            cell: { userCls: 'bold' }
+            cell: {
+                userCls: 'bold'
+            }
         },
         {
             text: 'Name',
             dataIndex: 'name',
 
             width: 100,
-            cell: { userCls: 'bold' }
+            cell: {
+                userCls: 'bold'
+            }
         },
         {
             text: 'Email',
@@ -32,61 +37,34 @@ Ext.define('newApp.view.user.grid.Grid', {
         {
             text: 'Phone',
             dataIndex: 'phone',
-
             width: 150
         },
         {
             text: 'Actions',
             iconCls: 'x-fa fa-image',
             flex: 1,
-
             cell: {
                 xtype: "widgetcell",
                 widget: {
                     xtype: "button",
-
                     text: '',
-
                     menu: {
                         xtype: 'menu',
-                        items: [{
-                            text: 'Edit',
-
-                            iconCls: 'x-fa fa-edit',
-                            handler: 'editUserData',
-                            // data: this
-
-
-                        }, {
-                            text: 'Delete',
-                            iconCls: 'x-fa fa-trash',
-                            handler: "deleteUser",
-
-
-                        },]
-
-                    },
-
+                        items: [
+                            {
+                                text: 'Edit',
+                                iconCls: 'x-fa fa-edit',
+                                handler: 'editUserData'
+                            },
+                            {
+                                text: 'Delete',
+                                iconCls: 'x-fa fa-trash',
+                                handler: "deleteUser"
+                            }
+                        ]
+                    }
                 }
             }
-
-        },
-        
-        
-    ],
- 
-    listeners:{
-        select: function(){
-       this._plugins[0]._totalPages=10
-            console.log(this)
-
-            // Ext.StoreManager.lookup('usersStore').totalCount=10
-            // console.log( Ext.StoreManager.lookup('usersStore'))
-            // console.log(Ext.StoreManager.lookup('usersStore').load())
-            // console.log( Ext.StoreManager.lookup('usersStore'))
-
         }
-    }
-    
-  
+    ]
 });
